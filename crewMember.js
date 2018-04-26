f5efee28ab4bcb1bda570bb02dda294c8af08795
victoria-huang <vctrah@gmail.com>
2018-04-26 15:27:50 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,34 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = "Looking for a Rig";
+  }
+
+  // setCurrentShip(ship) {
+  //   this.currentShip = ship;
+  // }
+
+  engageWarpDrive() {
+    if (this.currentShip === "Looking for a Rig" || this.position !== "Pilot") {
+      return "had no effect";
+    } else {
+      this.currentShip.warpDrive = "engaged";
+    }
+  }
+
+  chargePhasers() {
+    if (this.currentShip === "Looking for a Rig" || this.position !== "Gunner") {
+      return "had no effect";
+    } else {
+      this.currentShip.phasersCharge = "charged";
+    }
+  }
+
+  setsInvisibility() {
+    if (this.currentShip === "Looking for a Rig" || this.position !== "Defender") {
+      return "had no effect";
+    } else {
+      this.currentShip.cloaked = true;
+    }
+  }
+}
