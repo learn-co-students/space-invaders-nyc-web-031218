39292c7f4e214703caf1686df15b4af2c88b045b
github username <jeremyagottfried@gmail.com>
2018-04-26 15:21:33 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,36 @@
-class CrewMember {}
+let crewId = 0
+class CrewMember {
+  constructor(position) {
+    this.position = position
+    this.id = crewId++
+    store.crew.push(this)
+    this.currentShip = 'Looking for a Rig'
+  }
+  chargePhasers() {
+    if (this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    }
+    else {
+    this.currentShip.phasers = "charged"
+    }
+  }
+  engageWarpDrive() {
+    if (this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    }
+    else if (this.position === "Pilot") {
+    this.currentShip.warpDrive = "engaged"
+
+    }
+  }
+  setsInvisibility() {
+    if (this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    }
+    else if (this.position === "Defender"){
+
+    this.currentShip.cloaked = true
+    }
+  }
+
+}
